77787b16e0124d7bbb1af43e1b3d62b3df722fe7
Orlando Diaz <orlandodiazconde@gmail.com>
2023-12-05 16:04:54 -0500

    Add ch5 exercises (filters)

---
+++ ch5/ex1/src/main/java/com/example/filter/AuthenticationLoggingFilter.java
@@ -0,0 +1,19 @@
+package com.example.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class AuthenticationLoggingFilter implements Filter {
+    private final Logger logger = Logger.getLogger(AuthenticationLoggingFilter.class.getName());
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        var httpRequest = (HttpServletRequest) request;
+        var requestId = httpRequest.getHeader("Request-Id");
+        logger.info("Successfully authenticated request with id " + requestId);
+        filterChain.doFilter(request, response);
+    }
+}
---
+++ ch5/ex3/src/main/java/com/example/config/ProjectConfig.java
@@ -0,0 +1,20 @@
+package com.example.config;
+
+import com.example.filter.AuthenticationLoggingFilter;
+import com.example.filter.RequestValidationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.addFilterBefore(new RequestValidationFilter(), BasicAuthenticationFilter.class)
+                .addFilterAfter(new AuthenticationLoggingFilter(), BasicAuthenticationFilter.class)
+                .authorizeHttpRequests(c -> c.anyRequest().permitAll());
+        return http.build();
+    }
+}
